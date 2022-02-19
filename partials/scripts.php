4bf4b9e52a11580abda476a3b03e420433119b60
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-19 13:16:31 +0300

    Implement Scripts - Inject Swal

--- partials/scripts.php
+++ partials/scripts.php
@@ -13,3 +13,64 @@
 <!-- Sweet Alerts -->
 <script src="assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
 <!-- Init Alerts -->
+<!-- Init Sweet Alerts -->
+<?php if (isset($success)) { ?>
+    <!-- Pop Success Alert -->
+    <script>
+        Swal.fire({
+            title: 'Success',
+            html: '<?php echo $success; ?>',
+            timer: 500,
+            type: "success",
+            onBeforeOpen: () => {
+                Swal.showLoading()
+                timerInterval = setInterval(() => {
+                    Swal.getContent().querySelector('strong')
+                        .textContent = Swal.getTimerLeft()
+                }, 100)
+            },
+            onClose: () => {
+                clearInterval(timerInterval)
+            }
+        }).then((result) => {
+            if (
+                result.dismiss === Swal.DismissReason.timer
+            ) {
+                console.log('I was closed by the timer')
+            }
+        })
+    </script>
+
+<?php }
+if (isset($err)) { ?>
+    <script>
+        /* Pop Error Message */
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+        });
+        Toast.fire({
+            type: 'error',
+            title: '<?php echo $err; ?>',
+        })
+    </script>
+
+<?php }
+if (isset($info)) { ?>
+    <script>
+        /* Pop Warning  */
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+        });
+        Toast.fire({
+            type: 'info',
+            title: '<?php echo $info; ?>',
+        })
+    </script>
+
+<?php }
