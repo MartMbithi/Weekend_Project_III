dc377c1b8d4f8f4dc047ff82482f981d44710234
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-19 13:44:31 +0300

    Segment Main Dashboard Pages

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,129 +1,196 @@
-<?php if ($_SESSION['user_access_level'] == 'admin') { ?>
-    <nav class="navbar navbar-main navbar-expand-lg navbar-dark nav_bg navbar-border" id="navbar-main">
-        <div class="container-fluid">
-            <!-- Brand + Toggler (for mobile devices) -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-main-collapse" aria-controls="navbar-main-collapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- User's navbar -->
-            <div class="navbar-user d-lg-none ml-auto">
-                <ul class="navbar-nav flex-row align-items-center">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link nav-link-icon sidenav-toggler" data-action="sidenav-pin" data-target="#sidenav-main"><i class="far fa-bars"></i></a>
-                    </li>
+<header id="topnav">
+    <div class="topbar-main">
+        <div class="container">
 
-                    <li class="nav-item dropdown dropdown-animate">
-                        <a class="nav-link pr-lg-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="avatar avatar-sm rounded-circle">
-                                <img alt="Image placeholder" src="assets/img/users/no-profile.png">
-                            </span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right dropdown-menu-arrow">
-                            <h6 class="dropdown-header px-0">Hi, <?php echo $_SESSION['user_name']; ?></h6>
-                            <a href="profile" class="dropdown-item">
-                                <i class="far fa-user"></i>
-                                <span>My profile</span>
-                            </a>
-                            <a href="logout" class="dropdown-item">
-                                <i class="far fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
+            <!-- LOGO -->
+            <div class="topbar-left">
+                <a href="dashboard" class="logo">
+                    <span>One Hen MIS</span>
+                </a>
             </div>
-            <!-- Navbar nav -->
-            <div class="collapse navbar-collapse navbar-collapse-fade" id="navbar-main-collapse">
+            <!-- End Logo container-->
 
-                <ul class="navbar-nav ml-lg-auto align-items-center d-none d-lg-flex">
-                    <li class="nav-item dropdown dropdown-animate">
-                        <a class="nav-link pr-lg-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <div class="media media-pill align-items-center">
-                                <span class="avatar rounded-circle">
-                                    <img alt="Image placeholder" src="assets/img/users/no-profile.png">
-                                </span>
-                                <div class="ml-2 d-none d-lg-block">
-                                    <span class="mb-0 text-sm  font-weight-bold"><?php echo $_SESSION['user_name']; ?></span>
-                                </div>
+
+            <div class="menu-extras navbar-topbar">
+
+                <ul class="list-inline float-right mb-0">
+
+                    <li class="list-inline-item">
+                        <!-- Mobile menu toggle-->
+                        <a class="navbar-toggle">
+                            <div class="lines">
+                                <span></span>
+                                <span></span>
+                                <span></span>
                             </div>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right dropdown-menu-arrow">
-                            <a href="profile" class="dropdown-item">
-                                <i class="far fa-user"></i>
-                                <span>My profile</span>
-                            </a>
-                            <a href="logout" class="dropdown-item">
-                                <i class="far fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-<?php } else { ?>
-    <nav class="navbar navbar-main navbar-expand-lg navbar-dark nav_bg navbar-border" id="navbar-main">
-        <div class="container-fluid">
-            <!-- Brand + Toggler (for mobile devices) -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-main-collapse" aria-controls="navbar-main-collapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- User's navbar -->
-            <div class="navbar-user d-lg-none ml-auto">
-                <ul class="navbar-nav flex-row align-items-center">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link nav-link-icon sidenav-toggler" data-action="sidenav-pin" data-target="#sidenav-main"><i class="far fa-bars"></i></a>
+                        <!-- End mobile menu toggle-->
                     </li>
 
-                    <li class="nav-item dropdown dropdown-animate">
-                        <a class="nav-link pr-lg-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="avatar avatar-sm rounded-circle">
-                                <img alt="Image placeholder" src="assets/img/users/no-profile.png">
-                            </span>
+
+                    <li class="list-inline-item dropdown notification-list">
+                        <a class="nav-link waves-effect waves-light right-bar-toggle" href="javascript:void(0);">
+                            <i class="zmdi zmdi-format-subject noti-icon"></i>
                         </a>
-                        <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right dropdown-menu-arrow">
-                            <h6 class="dropdown-header px-0">Hi, <?php echo $_SESSION['user_name']; ?></h6>
-                            <a href="user_profile" class="dropdown-item">
-                                <i class="far fa-user"></i>
-                                <span>My profile</span>
-                            </a>
-                            <a href="logout" class="dropdown-item">
-                                <i class="far fa-sign-out-alt"></i>
-                                <span>Logout</span>
-                            </a>
-                        </div>
                     </li>
-                </ul>
-            </div>
-            <!-- Navbar nav -->
-            <div class="collapse navbar-collapse navbar-collapse-fade" id="navbar-main-collapse">
 
-                <ul class="navbar-nav ml-lg-auto align-items-center d-none d-lg-flex">
-                    <li class="nav-item dropdown dropdown-animate">
-                        <a class="nav-link pr-lg-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <div class="media media-pill align-items-center">
-                                <span class="avatar rounded-circle">
-                                    <img alt="Image placeholder" src="assets/img/users/no-profile.png">
-                                </span>
-                                <div class="ml-2 d-none d-lg-block">
-                                    <span class="mb-0 text-sm  font-weight-bold"><?php echo $_SESSION['user_name']; ?></span>
-                                </div>
+                    <li class="list-inline-item dropdown notification-list">
+                        <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                            <img src="assets/images/no-profile.png" alt="user" class="rounded-circle">
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right profile-dropdown " aria-labelledby="Preview">
+                            <!-- item-->
+                            <div class="dropdown-item noti-title">
+                                <h5 class="text-overflow"><small>Welcome ! <?php echo $_SESSION['user_name'] ?></small> </h5>
                             </div>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right dropdown-menu-arrow">
-                            <a href="user_profile" class="dropdown-item">
-                                <i class="far fa-user"></i>
-                                <span>My profile</span>
+
+                            <!-- item-->
+                            <a href="settings.php" class="dropdown-item notify-item">
+                                <i class="zmdi zmdi-settings"></i> <span>Settings</span>
                             </a>
-                            <a href="logout" class="dropdown-item">
-                                <i class="far fa-sign-out-alt"></i>
-                                <span>Logout</span>
+                            <!-- item-->
+                            <a href="logout" class="dropdown-item notify-item">
+                                <i class="zmdi zmdi-power"></i> <span>Logout</span>
                             </a>
+
                         </div>
                     </li>
+
                 </ul>
+
+            </div> <!-- end menu-extras -->
+            <div class="clearfix"></div>
+
+        </div> <!-- end container -->
+    </div>
+    <!-- end topbar-main -->
+
+
+    <div class="navbar-custom">
+        <div class="container">
+            <div id="navigation">
+                <!-- Navigation Menu-->
+                <ul class="navigation-menu">
+                    <li>
+                        <a href="index.html"><i class="zmdi zmdi-view-dashboard"></i> <span> Dashboard </span> </a>
+                    </li>
+                    <li class="has-submenu">
+                        <a href="#"><i class="zmdi zmdi-format-underlined"></i> <span> User Interface </span> </a>
+                        <ul class="submenu megamenu">
+                            <li>
+                                <ul>
+                                    <li><a href="ui-buttons.html">Buttons</a></li>
+                                    <li><a href="ui-cards.html">Cards</a></li>
+                                    <li><a href="ui-dropdowns.html">Dropdowns</a></li>
+                                    <li><a href="ui-checkbox-radio.html">Checkboxs-Radios</a></li>
+                                    <li><a href="ui-navs.html">Navs</a></li>
+                                    <li><a href="ui-progress.html">Progress</a></li>
+                                    <li><a href="ui-modals.html">Modals</a></li>
+                                    <li><a href="ui-alerts.html">Alerts</a></li>
+                                    <li><a href="ui-bootstrap.html">Bootstrap UI</a></li>
+                                    <li><a href="ui-typography.html">Typography</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li><a href="ui-notification.html">Notification</a></li>
+                                    <li><a href="ui-carousel.html">Carousel</a></li>
+                                    <li><a href="components-grid.html">Grid</a></li>
+                                    <li><a href="components-range-sliders.html">Range sliders</a></li>
+                                    <li><a href="components-sweet-alert.html">Sweet Alerts</a></li>
+                                    <li><a href="components-ratings.html">Ratings</a></li>
+                                    <li><a href="components-treeview.html">Treeview</a></li>
+                                    <li><a href="components-tour.html">Tour</a></li>
+                                    <li><a href="widgets-tiles.html">Tile Box</a></li>
+                                    <li><a href="widgets-charts.html">Chart Widgets</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li class="has-submenu">
+                        <a href="#"><i class="zmdi zmdi-album"></i> <span> Icons </span> </a>
+                        <ul class="submenu">
+                            <li><a href="icons-materialdesign.html">Material Design</a></li>
+                            <li><a href="icons-ionicons.html">Ion Icons</a></li>
+                            <li><a href="icons-fontawesome.html">Font awesome</a></li>
+                            <li><a href="icons-themify.html">Themify Icons</a></li>
+                            <li><a href="icons-simple-line.html">Simple line Icons</a></li>
+                            <li><a href="icons-weather.html">Weather Icons</a></li>
+                            <li><a href="icons-pe7.html">PE7 Icons</a></li>
+                            <li><a href="icons-typicons.html">Typicons</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-submenu">
+                        <a href="#"><i class="zmdi zmdi-collection-text"></i><span> Forms </span> </a>
+                        <ul class="submenu">
+                            <li><a href="form-elements.html">General Elements</a></li>
+                            <li><a href="form-advanced.html">Advanced Form</a></li>
+                            <li><a href="form-validation.html">Form Validation</a></li>
+                            <li><a href="form-pickers.html">Form Pickers</a></li>
+                            <li><a href="form-wizard.html">Form Wizard</a></li>
+                            <li><a href="form-mask.html">Form Masks</a></li>
+                            <li><a href="form-uploads.html">Multiple File Upload</a></li>
+                            <li><a href="form-xeditable.html">X-editable</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-submenu">
+                        <a href="#"><i class="zmdi zmdi-format-list-bulleted"></i> <span> Tables </span> </a>
+                        <ul class="submenu">
+                            <li><a href="tables-basic.html">Basic Tables</a></li>
+                            <li><a href="tables-datatable.html">Data Table</a></li>
+                            <li><a href="tables-responsive.html">Responsive Table</a></li>
+                            <li><a href="tables-tablesaw.html">Tablesaw</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-submenu">
+                        <a href="#"><i class="zmdi zmdi-chart"></i><span> Charts </span> </a>
+                        <ul class="submenu">
+                            <li><a href="chart-flot.html">Flot Chart</a></li>
+                            <li><a href="chart-morris.html">Morris Chart</a></li>
+                            <li><a href="chart-chartjs.html">Chartjs</a></li>
+                            <li><a href="chart-peity.html">Peity Charts</a></li>
+                            <li><a href="chart-chartist.html">Chartist Charts</a></li>
+                            <li><a href="chart-c3.html">C3 Charts</a></li>
+                            <li><a href="chart-sparkline.html">Sparkline charts</a></li>
+                            <li><a href="chart-knob.html">Jquery Knob</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-submenu">
+                        <a href="#"><i class="zmdi zmdi-collection-item"></i> <span> Pages </span> </a>
+                        <ul class="submenu megamenu">
+                            <li>
+                                <ul>
+                                    <li><a href="calendar.html">Calendar</a></li>
+                                    <li><a href="pages-starter.html">Starter Page</a></li>
+                                    <li><a href="pages-login.html">Login</a></li>
+                                    <li><a href="pages-register.html">Register</a></li>
+                                    <li><a href="pages-recoverpw.html">Recover Password</a></li>
+                                    <li><a href="pages-lock-screen.html">Lock Screen</a></li>
+                                    <li><a href="pages-404.html">Error 404</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li><a href="pages-500.html">Error 500</a></li>
+                                    <li><a href="pages-timeline.html">Timeline</a></li>
+                                    <li><a href="pages-invoice.html">Invoice</a></li>
+                                    <li><a href="pages-pricing.html">Pricing</a></li>
+                                    <li><a href="pages-gallery.html">Gallery</a></li>
+                                    <li><a href="pages-maintenance.html">Maintenance</a></li>
+                                    <li><a href="pages-comingsoon.html">Coming Soon</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <!-- End navigation menu  -->
             </div>
         </div>
-    </nav>
-<?php } ?>
+    </div>
+</header>
