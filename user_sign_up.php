358777854e5e9afd6853ac4f8529dea901324bcb
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-21 05:30:07 +0300

    Implement User Sign Up Logic

--- user_sign_up.php
+++ user_sign_up.php
@@ -63,23 +63,31 @@ require_once 'config/config.php';
 require_once 'config/codeGen.php';
 
 /* Handle Login */
-if (isset($_POST['Login'])) {
-    $user_email = $_POST['user_email'];
+if (isset($_POST['Sign_Up'])) {
+    $user_access_level = $_GET['access'];;
+    $user_name = $_POST['user_name'];
+    $user_email  = $_POST['user_email'];
+    $user_phone_no = $_POST['user_phone_no'];
     $user_password = sha1(md5($_POST['user_password']));
-    $stmt = $mysqli->prepare("SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email=? AND user_password=?");
-    $stmt->bind_param('ss', $user_email, $user_password);
-    $stmt->execute();
-    $stmt->bind_result($user_id, $user_name, $user_email, $user_password);
-    $rs = $stmt->fetch();
 
-    /* Session Variables */
-    $_SESSION['user_id'] = $user_id;
-    $_SESSION['user_name'] = $user_name;
-
-    if ($rs) {
-        header("location:dashboard");
+    /* Persist */
+    $sql = "INSERT INTO users (user_name, user_email, user_phone_no, user_access_level, user_password)
+    VALUES(?,?,?,?,?)";
+    $prepare = $mysqli->prepare($sql);
+    $bind = $prepare->bind_param(
+        'sssss',
+        $user_name,
+        $user_email,
+        $user_phone_no,
+        $user_access_level,
+        $user_password
+    );
+    $prepare->execute();
+    if ($prepare) {
+        $_SESSION['success'] = 'Account Created, Proceed To Login';
+        header('Location: index');
     } else {
-        $err = "Access Denied Please Check Your Email Or Password";
+        $err  = "Failed!, Please Try Again";
     }
 }
 
