266bf3a3c2725b9a3869963d52f1a115f0c9e7d1
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-21 05:24:53 +0300

    Implement User - Customer And Supplier Sign In

---
+++ user_sign_up.php
@@ -0,0 +1,154 @@
+<?php
+/*
+ * Created on Sat Feb 19 2022
+ *
+ *  Devlan Agency - devlan.co.ke 
+ *
+ * hello@devlan.co.ke
+ *
+ *
+ * The Devlan End User License Agreement
+ *
+ * Copyright (c) 2022 Devlan Agency
+ *
+ * 1. GRANT OF LICENSE
+ * Devlan Agency hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ * install and activate this system on two separated computers solely for your personal and non-commercial use,
+ * unless you have purchased a commercial license from Devlan Agency. Sharing this Software with other individuals, 
+ * or allowing other individuals to view the contents of this Software, is in violation of this license.
+ * You may not make the Software available on a network, or in any way provide the Software to multiple users
+ * unless you have first purchased at least a multi-user license from Devlan Agency.
+ *
+ * 2. COPYRIGHT 
+ * The Software is owned by Devlan Agency and protected by copyright law and international copyright treaties. 
+ * You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ * 3. RESTRICTIONS ON USE
+ * You may not, and you may not permit others to
+ * (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ * (b) modify, distribute, or create derivative works of the Software;
+ * (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ * otherwise exploit the Software.  
+ *
+ * 4. TERM
+ * This License is effective until terminated. 
+ * You may terminate it at any time by destroying the Software, together with all copies thereof.
+ * This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ * Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ * 5. NO OTHER WARRANTIES. 
+ * DEVLAN AGENCY  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ * DEVLAN AGENCY SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ * SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ * ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ * INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ * SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ * THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ * HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ * 6. SEVERABILITY
+ * In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ * affect the validity of the remaining portions of this license.
+ *
+ * 7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL DEVLAN AGENCY  OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ * CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ * USE OF THE SOFTWARE, EVEN IF DEVLAN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ * IN NO EVENT WILL DEVLAN  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ * TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ */
+session_start();
+require_once 'config/config.php';
+require_once 'config/codeGen.php';
+
+/* Handle Login */
+if (isset($_POST['Login'])) {
+    $user_email = $_POST['user_email'];
+    $user_password = sha1(md5($_POST['user_password']));
+    $stmt = $mysqli->prepare("SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email=? AND user_password=?");
+    $stmt->bind_param('ss', $user_email, $user_password);
+    $stmt->execute();
+    $stmt->bind_result($user_id, $user_name, $user_email, $user_password);
+    $rs = $stmt->fetch();
+
+    /* Session Variables */
+    $_SESSION['user_id'] = $user_id;
+    $_SESSION['user_name'] = $user_name;
+
+    if ($rs) {
+        header("location:dashboard");
+    } else {
+        $err = "Access Denied Please Check Your Email Or Password";
+    }
+}
+
+/* Load Header Partial */
+require_once('partials/head.php');
+?>
+
+<body>
+
+    <div class="account-pages"></div>
+    <div class="clearfix"></div>
+    <div class="wrapper-page">
+
+        <div class="account-bg">
+            <div class="card-box mb-0">
+                <div class="text-center m-t-20">
+                    <a href="" class="logo">
+                        <span>Poultry Farm Information Management System</span>
+                    </a>
+                </div>
+                <div class="m-t-10 p-20">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <h6 class="text-muted text-uppercase m-b-0 m-t-0">Sign In</h6>
+                        </div>
+                    </div>
+                    <form class="m-t-20" method="POST">
+                        <div class="form-group row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" name="user_name" required placeholder="Full Name">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" name="user_phone_no" required placeholder="Phone Number">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-12">
+                                <input class="form-control" type="text" name="user_email" required placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-12">
+                                <input class="form-control" name="user_password" type="password" required placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group text-center row m-t-10">
+                            <div class="col-12">
+                                <button class="btn btn-success btn-block waves-effect waves-light" name="Sign_Up" type="submit">Sign Up</button>
+                            </div>
+                        </div>
+                        <div class="form-group row m-t-30 mb-0">
+                            <div class="col-12">
+                                <a href="index" class="text-muted"><i class="fa fa-lock m-r-5"></i> Already Has Account?</a>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <!-- end card-box-->
+    </div>
+    <!-- end wrapper page -->
+    <?php require('partials/scripts.php'); ?>
+
+</body>
+
+</html>
