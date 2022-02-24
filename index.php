24c300a5ea69be1b527a8b1df4beb4f9690a7b4f
Martin Mbithi <martdevelopers254@gmail.com>
2022-02-24 13:32:27 +0300

    Intergrate Landing Pages Logo

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
 
 
 /* Load Header Partial*/
-require_once('cms_assets/head.php');
+require_once('cms_partials/head.php');
 ?>
 
 <body>
@@ -78,227 +78,7 @@ require_once('cms_assets/head.php');
     <!-- preloader ending here -->
 
 
-    <!--search area-->
-    <div class="search-input">
-        <div class="search-close">
-            <i class="icofont-close-circled"></i>
-        </div>
-        <form>
-            <input type="text" name="text" placeholder="Search Heare">
-            <button class="search-btn" type="submit">
-                <i class="icofont-search-2"></i>
-            </button>
-        </form>
-    </div>
-    <!-- search area -->
-
-    <!-- Mobile Menu Start Here -->
-    <div class="mobile-menu transparent-header">
-        <nav class="mobile-header">
-            <div class="header-logo">
-                <a href="index.html"><img src="assets/images/logo/01.png" alt="logo"></a>
-            </div>
-            <div class="header-bar">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </nav>
-        <nav class="mobile-menu">
-            <div class="mobile-menu-area">
-                <div class="mobile-menu-area-inner">
-                    <ul class="lab-ul">
-                        <li class="active">
-                            <a href="#0">Home</a>
-                            <ul class="lab-ul">
-                                <li><a href="index.html">Home Page One</a></li>
-                                <li><a href="index-2.html">Home Page Two</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="about.html">About</a></li>
-                        <li>
-                            <a href="#0">Pages</a>
-                            <ul class="lab-ul">
-                                <li><a href="team.html">Team Membar</a></li>
-                                <li><a href="faq-page.html">Faq Page</a></li>
-                                <li><a href="404.html">404 Page</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#0">Blog</a>
-                            <ul class="lab-ul">
-                                <li><a href="blog.html">Blog</a></li>
-                                <li><a href="blog-single.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#0">Shop</a>
-                            <ul class="lab-ul">
-                                <li><a href="product-page.html">Products Page</a></li>
-                                <li><a href="product-single.html">Products Details</a></li>
-                                <li><a href="cart-page.html">Cart Page</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="contact.html">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-    <!-- Mobile Menu Ending Here -->
-
-    <!-- desktop menu start here -->
-    <header class="header-section">
-        <div class="header-top">
-            <div class="container">
-                <div class="row justify-content-center align-items-center">
-                    <div class="col-lg-5 col-12">
-                        <div class="logo py-2">
-                            <a href="index.html"><img src="assets/images/logo/01.png" alt="logo"></a>
-                        </div>
-                    </div>
-                    <div class="col-lg-7 col-12">
-                        <div class="ht-left">
-                            <ul class="lab-ul d-flex flex-wrap justify-content-end">
-                                <li class="d-flex flex-wrap align-items-center">
-                                    <div class="ht-add-thumb mr-2">
-                                        <img src="assets/images/header/01.png" alt="address">
-                                    </div>
-                                    <div class="ht-add-content">
-                                        <span>72AH, Victoria,</span>
-                                        <span class="d-block text-bold">Collins Street West USA</span>
-                                    </div>
-                                </li>
-                                <li class="d-flex flex-wrap align-items-center">
-                                    <div class="ht-add-thumb mr-2">
-                                        <img src="assets/images/header/02.png" alt="email">
-                                    </div>
-                                    <div class="ht-add-content">
-                                        <span>Send Mail </span>
-                                        <span class="d-block text-bold">murgifarm@gmail.com</span>
-                                    </div>
-                                </li>
-                                <li class="d-flex flex-wrap align-items-center">
-                                    <div class="ht-add-thumb mr-2">
-                                        <img src="assets/images/header/03.png" alt="call">
-                                    </div>
-                                    <div class="ht-add-content">
-                                        <span>Make Call </span>
-                                        <span class="d-block text-bold">+88-6487-5962-563</span>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="header-bottom bg-theme">
-            <div class="header-area">
-                <div class="container">
-                    <div class="primary-menu">
-                        <div class="main-area w-100">
-                            <div class="main-menu d-flex flex-wrap align-items-center justify-content-between w-100">
-                                <div class="logo">
-                                    <a href="index.html"><img src="assets/images/logo/02.png" alt="logo"></a>
-                                </div>
-                                <ul class="lab-ul">
-                                    <li class="active">
-                                        <a href="index.html">Home</a>
-                                        <ul class="lab-ul">
-                                            <li><a href="index.html" class="active">Home Page One</a></li>
-                                            <li><a href="index-2.html">Home Page Two</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="about.html">About</a></li>
-                                    <li>
-                                        <a href="#0">Pages</a>
-                                        <ul class="lab-ul">
-                                            <li><a href="team.html">Team Membar</a></li>
-                                            <li><a href="faq-page.html">Faq Page</a></li>
-                                            <li><a href="404.html">404 Page</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#0">Blog</a>
-                                        <ul class="lab-ul">
-                                            <li><a href="blog.html">Blog</a></li>
-                                            <li><a href="blog-single.html">Blog Details</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#0">Shop</a>
-                                        <ul class="lab-ul">
-                                            <li><a href="product-page.html">Products Page</a></li>
-                                            <li><a href="product-single.html">Products Details</a></li>
-                                            <li><a href="cart-page.html">Cart Page</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#0">Gallery</a>
-                                        <ul class="lab-ul">
-                                            <li><a href="gallery-1.html">Gallery 1</a></li>
-                                            <li><a href="gallery-2.html">Gallery 2</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                </ul>
-                                <ul class="lab-ul search-cart">
-                                    <li>
-                                        <div class="cart-option">
-                                            <i class="icofont-cart-alt"></i>
-                                            <div class="cart-content">
-                                                <div class="cart-item">
-                                                    <div class="cart-img">
-                                                        <a href="#"><img src="assets/images/products/product/01.png" alt="cart"></a>
-                                                    </div>
-                                                    <div class="cart-des">
-                                                        <a href="#">Product Title Here</a>
-                                                        <p>$20.00</p>
-                                                    </div>
-                                                    <div class="cart-btn">
-                                                        <a href="#"><i class="icofont-close-circled"></i></a>
-                                                    </div>
-                                                </div>
-                                                <div class="cart-item">
-                                                    <div class="cart-img">
-                                                        <a href="#"><img src="assets/images/products/product/02.png" alt="cart"></a>
-                                                    </div>
-                                                    <div class="cart-des">
-                                                        <a href="#">Product Title Here</a>
-                                                        <p>$20.00</p>
-                                                    </div>
-                                                    <div class="cart-btn">
-                                                        <a href="#"><i class="icofont-close-circled"></i></a>
-                                                    </div>
-                                                </div>
-                                                <div class="cart-bottom">
-                                                    <div class="cart-subtotal">
-                                                        <p>Total: <b class="float-right">$40.00</b></p>
-                                                    </div>
-                                                    <div class="cart-action">
-                                                        <a href="#" class="lab-btn"><span>View Cart</span></a>
-                                                        <a href="#" class="lab-btn"><span>Check Out</span></a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="search-option">
-                                            <i class="icofont-search-2"></i>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-    <!-- desktop menu ending here -->
-
+    <?php require_once('cms_partials/navbar.php'); ?>
     <!-- Banner Section Start Here -->
     <section class="banner">
         <div class="banner-slider">
@@ -1239,9 +1019,9 @@ require_once('cms_assets/head.php');
     <!-- Feature section ending here -->
 
     <!-- footer section start here -->
-    <?php require_once('cms_assets/footer.php'); ?>
+    <?php require_once('cms_partials/footer.php'); ?>
     <!-- footer section start here -->
-    <?php require_once('cms_assets/scripts.php'); ?>
+    <?php require_once('cms_partials/scripts.php'); ?>
 </body>
 
 </html>
